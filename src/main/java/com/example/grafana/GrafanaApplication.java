3f36b7eb4b732bf10a815a42c8c438760bc74245
Yuya Kusaka <10cherry07@gmail.com>
2025-01-26 06:26:20 +0900

    Initial Commit

---
+++ src/main/java/com/example/grafana/GrafanaApplication.java
@@ -0,0 +1,12 @@
+package com.example.grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrafanaApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/grafana/GrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
